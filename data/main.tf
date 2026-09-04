4535050cc83a6f2fb731d2f612ced62b0072cf63
Avinash-DevopsEr <avi.mk1998@gmail.com>
2026-09-05 00:24:50 +0530

    Refactor VPC and subnet resources to use explicit resource definitions and update references in security group, ALB, and Auto Scaling configurations

--- data/main.tf
+++ data/main.tf
@@ -1,18 +1,100 @@
 #Using default vpc
-data "aws_vpc" "default" {
-  default = true
+resource "aws_vpc" "vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "dev-vpc"
+  }
 }
 #Using default subnets
-data "aws_subnets" "default" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
+resource "aws_subnet" "public_1" {
+  cidr_block = "10.0.0.0/20"
+  vpc_id = aws_vpc.default.id
+  availability_zone = "ap-south-1a"
+  tags = {
+    Name = "public-subnet-1"
   }
+}
+resource "aws_subnet" "public_2" {
+  cidr_block = "10.0.32.0/20"
+  vpc_id = aws_vpc.default.id
+  availability_zone = "ap-south-1a"
+  tags = {
+    Name = "public-subnet-2"
+  }
+}
+
+resource "aws_subnet" "private" {
+  cidr_block = "10.0.16.0/20"
+  vpc_id = aws_vpc.default.id
+  availability_zone = "ap-south-1b"
+  tags = {
+    Name = "private-subnet"
+  }
+}
+
+resource "aws_internet_gateway" "igw"{
+    vpc_id = aws_vpc.vpc.id
+    tags = {
+        Name = "igw"
+    }
+}
+
+resource "aws_eip" "nat_eip"{
+    domain = vpc
+    tags = {
+        Name = "nat-eip"
+    }
+}
+
+resource "aws_nat_gateway" "nat"{
+    allocation_id = aws_eip.nat_eip.id
+    subnet_id = aws_subnet.public.id
+    tags = {
+        Name = "nat"
+    }
+}
+
+resource "aws_route_table" "public_rt"{
+    vpc_id = aws_vpc.vpc.id
+    route{
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.igw.id
+    }
+    tags = {
+        Name = "public-rt"
+    }
+}
+
+resource "aws_route_table_association" "public_rt_asso"{
+    subnet_id = aws_subnet.public.id
+    route_table_id = aws_route_table.public_rt.id
+    tags = {
+        Name = "public-rt-asso"
+    }
+}
+
+resource "aws_route_table" "private_rt"{
+    vpc_id = aws_vpc.vpc.id
+    route{
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_nat_gateway.igw.id
+    }
+    tags = {
+        Name = "private-rt"
+    }
+}
+
+resource "aws_route_table_association" "private_rt_asso"{
+    subnet_id = aws_subnet.private.id
+    route_table_id = aws_route_table.private_rt.id
+    tags = {
+        Name = "private-rt-asso"
+    }
 }
 #Creating security group
 resource "aws_security_group" "alb_sg"{
     name = "alb_sg"
-    vpc_id = data.aws_vpc.default.id
+    vpc_id = aws_vpc.vpc.id
     ingress{
         from_port = 80
         to_port = 80
@@ -40,7 +122,7 @@ resource "aws_lb_target_group" "tg"{
     name = "tg"
     port = 80
     protocol = "HTTP"
-    vpc_id = data.aws_vpc.default.id
+    vpc_id = aws_vpc.vpc.id
     health_check {
         path = "/"
     }
@@ -51,7 +133,7 @@ resource "aws_lb" "app_lb"{
     load_balancer_type = "application"
     internal = false
     security_groups = [aws_security_group.alb_sg.id]
-    subnets = data.aws_subnets.default.ids
+    subnets = [aws_subnet.public_1.id,aws_subnet.public_2.id]
 }
 #Creating Listener
 resource "aws_alb_listener" "listener"{
@@ -80,7 +162,7 @@ resource "aws_autoscaling_group" "asg"{
     max_size = 4
     health_check_type = "ELB"
     target_group_arns = [aws_lb_target_group.tg.arn]
-    vpc_zone_identifier = data.aws_subnets.default.ids
+    vpc_zone_identifier = [aws_subnet.public_1.id,aws_subnet.public_2.id]
     launch_template{
         id = aws_launch_template.lt.id
         version = "$Latest"
