550eacf6774e392f94ee5ae5302046340927c955
Avinash-DevopsEr <avi.mk1998@gmail.com>
2026-09-04 23:59:10 +0530

    Fix ALB name format and correct health check type in Auto Scaling group

--- data/main.tf
+++ data/main.tf
@@ -47,7 +47,7 @@ resource "aws_lb_target_group" "tg"{
 }
 #Creating ALB
 resource "aws_lb" "app_lb"{
-    name = "app_lb"
+    name = "app-lb"
     load_balancer_type = "application"
     internal = false
     security_groups = [aws_security_group.alb_sg.id]
@@ -78,8 +78,8 @@ resource "aws_autoscaling_group" "asg"{
     desired_capacity = 2
     min_size = 2
     max_size = 4
-    health_check_type = group
-    target_group_arns = [alb_lb_target_group.tg.arn]
+    health_check_type = "ELB"
+    target_group_arns = [aws_lb_target_group.tg.arn]
     vpc_zone_identifier = [data.aws_subnets.default.ids]
     launch_template{
         id = aws_launch_template.lt.id
